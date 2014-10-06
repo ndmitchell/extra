c7833ae9ca219a79abdb4a1eae606356daa12dff
Neil Mitchell <ndmitchell@gmail.com>
2014-10-06 22:18:39 +0100

    Also generate a test

--- Generate.hs
+++ Generate.hs
@@ -5,6 +5,7 @@ import Data.List.Extra
 import Control.Monad
 import System.FilePath
 import Data.Char
+import Data.Maybe
 
 
 main :: IO ()
@@ -15,13 +16,21 @@ main = do
         src <- readFile $ joinPath (wordsBy (== '.') mod) <.> "hs"
         let funcs = filter validIdentifier $ takeWhile (/= "where") $ words $ reps ',' ' ' $
                     unlines $ filter (not . isPrefixOf "--" . trim) $ lines src
-        return (mod, funcs)
+        let tests = mapMaybe (stripPrefix "-- > ") $ lines src
+        return (mod, funcs, tests)
     writeFile "Extra.hs" $ unlines $
         ["module Extra("] ++
-        concat [["    -- * " ++ mod, "    " ++ unwords (map (++",") funs)] | (mod,funs) <- ifaces] ++
+        concat [["    -- * " ++ mod, "    " ++ unwords (map (++",") funs)] | (mod,funs,_) <- ifaces] ++
         ["    ) where"
         ,""] ++
         ["import " ++ x | x <- mods]
+    writeFile "Test.hs" $ unlines $
+        ["module Test(main) where"] ++
+        ["import " ++ x | x <- mods] ++
+        ["main :: IO ()"
+        ,"main = do"] ++
+        ["  " ++ t | (_,_,ts) <- ifaces, t <- ts]
+
 
 validIdentifier (x:xs) = isLower x && (x:xs) /= "module"
 validIdentifier _ = False
