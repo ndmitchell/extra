c6904cda3d0a6a0d26eed7cdb7f1887c07e9786f
Neil Mitchell <ndmitchell@gmail.com>
2014-10-07 13:17:06 +0100

    Fix the travis script to detect not rerunning the generator

--- travis.hs
+++ travis.hs
@@ -1,6 +1,11 @@
 
 import Neil
+import System.IO.Extra
 
 main = do
+    let files = ["src/Extra.hs","src/Test.hs"]
+    before <- mapM readFile' files
     cmd "runhaskell -isrc Generate"
+    after <- mapM readFile' files
+    when (before /= after) $ error "Generator changed some files, that's a bug"
     cmd "runhaskell -isrc Test"
