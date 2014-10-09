4ed4c0c086caa8578ae0fcbec3e5835e3223fea4
Neil Mitchell <ndmitchell@gmail.com>
2014-10-09 18:45:07 +0100

    Add tests for word1 and readFile' being exported

--- test/Test.hs
+++ test/Test.hs
@@ -5,11 +5,13 @@ import TestGen
 import TestUtil
 
 -- Check that we managed to export everything
-import Extra(Seconds, whenJust, (&&^), system_)
-_unused1 x = whenJust x
-_unused2 x = (&&^) x
-_unused3 x = system_ x
-_unused4 x = x :: Seconds
+import Extra(Seconds, whenJust, (&&^), system_, word1, readFile')
+_unused1 x = whenJust
+_unused2 x = (&&^)
+_unused3 x = system_
+_unused4 x = word1
+_unused5 x = readFile'
+_unused6 x = x :: Seconds
 
 
 main :: IO ()
