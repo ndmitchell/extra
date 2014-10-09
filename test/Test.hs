08fb0a5d32cd6de7e61750f40f2ab0be3aa9c660
Neil Mitchell <ndmitchell@gmail.com>
2014-10-09 18:43:04 +0100

    In the test check that a minimum set of symbols got exported

--- test/Test.hs
+++ test/Test.hs
@@ -4,5 +4,13 @@ module Test(main) where
 import TestGen
 import TestUtil
 
+-- Check that we managed to export everything
+import Extra(Seconds, whenJust, (&&^), system_)
+_unused1 x = whenJust x
+_unused2 x = (&&^) x
+_unused3 x = system_ x
+_unused4 x = x :: Seconds
+
+
 main :: IO ()
 main = runTests $ tests
