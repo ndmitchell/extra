e083611ced53144daad1132563042c1bd41fe8e0
Neil Mitchell <ndmitchell@gmail.com>
2014-10-09 18:42:48 +0100

    Use travis to check that the files really did regenerate

--- travis.hs
+++ travis.hs
@@ -6,6 +6,7 @@ import System.Process.Extra
 main = do
     let files = ["src/Extra.hs","test/TestGen.hs"]
     before <- mapM readFile' files
+    mapM_ (`appendFile` " ") files -- ensure that if they don't regen, it will error
     system_ "runhaskell -isrc Generate"
     after <- mapM readFile' files
     when (before /= after) $ error "Generator changed some files, that's a bug"
