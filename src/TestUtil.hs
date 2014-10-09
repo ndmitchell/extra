5740b0e6ea0b5fb5b1fd68932296f333f2dd463c
Neil Mitchell <ndmitchell@gmail.com>
2014-10-09 18:15:08 +0100

    Support tests defined as == error

--- src/TestUtil.hs
+++ src/TestUtil.hs
@@ -1,11 +1,19 @@
-module TestUtil(test) where
+
+module TestUtil(test, erroneous) where
 
 import Test.QuickCheck
 import Test.QuickCheck.Test hiding (test)
 import Control.Monad
+import Control.Exception.Extra
+import Data.Either.Extra
+import System.IO.Unsafe
+
 
 test :: Testable prop => String -> prop -> IO ()
 test msg prop = do
     putStrLn msg
     r <- quickCheckResult prop
     unless (isSuccess r) $ error "Test failed"
+
+erroneous :: a -> Bool
+erroneous x = unsafePerformIO $ fmap isLeft $ try_ $ evaluate x
