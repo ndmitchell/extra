36601a46d28ff81285e273ce3c2ef2b9d620c607
Neil Mitchell <ndmitchell@gmail.com>
2014-10-12 21:59:57 +0100

    Track if anything changes better

--- travis.hs
+++ travis.hs
@@ -5,13 +5,6 @@ import System.Process.Extra
 
 main = do
     let files = ["src/Extra.hs","test/TestGen.hs"]
-    before <- mapM readFile' files
-    mapM_ (`appendFile` " ") files -- ensure that if they don't regen, it will error
     system_ "runhaskell -isrc Generate"
-    after <- mapM readFile' files
-    when (before /= after) $ error "Generator changed some files, that's a bug"
-
-    src <- systemOutput_ "git checkout" -- should have no stdout
-    when (lines src /= []) $ error $ "generating changed something!" ++ src
-
+    system_ "git diff --exit-code"
     system_ "runhaskell -isrc -itest Test"
