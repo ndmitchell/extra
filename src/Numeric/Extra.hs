7928b54d7a82b47b246fc55beac696ff9d403a3f
Neil Mitchell <ndmitchell@gmail.com>
2014-10-18 13:22:21 +0100

    Docs for Numeric.Extra

--- src/Numeric/Extra.hs
+++ src/Numeric/Extra.hs
@@ -11,6 +11,11 @@ import Control.Arrow
 ---------------------------------------------------------------------
 -- Data.String
 
+-- | Show a number to a number of decimal places.
+--
+-- > showDP 4 pi == "3.1416"
+-- > showDP 0 pi == "3"
+-- > showDP 2 3  == "3.00"
 showDP :: RealFloat a => Int -> a -> String
 showDP n x = a ++ (if n > 0 then "." else "") ++ b ++ replicate (n - length b) '0'
     where (a,b) = second (drop 1) $ break (== '.') $ showFFloat (Just n) x ""
@@ -19,15 +24,19 @@ showDP n x = a ++ (if n > 0 then "." else "") ++ b ++ replicate (n - length b) '
 ---------------------------------------------------------------------
 -- Numeric
 
+-- | Specialised numeric conversion.
 intToDouble :: Int -> Double
 intToDouble = fromIntegral
 
+-- | Specialised numeric conversion.
 intToFloat :: Int -> Float
 intToFloat = fromIntegral
 
+-- | Specialised numeric conversion.
 floatToDouble :: Float -> Double
 floatToDouble = realToFrac
 
+-- | Specialised numeric conversion.
 doubleToFloat :: Double -> Float
 doubleToFloat = realToFrac
 
