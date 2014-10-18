c763849f09a24656a92dc21cf0c73d1235803f23
Neil Mitchell <ndmitchell@gmail.com>
2014-10-18 13:05:04 +0100

    More docs for Either

--- src/Data/Either/Extra.hs
+++ src/Data/Either/Extra.hs
@@ -8,7 +8,16 @@ module Data.Either.Extra(
 
 import Data.Either
 
+-- | The 'fromLeft' function extracts the element out of a 'Left' and
+--   throws an error if its argument is 'Right'.
+--   Much like 'fromJust', using this function in polished code is usually a bad idea.
+fromLeft :: Either l r -> l
 fromLeft (Left x) = x
+
+-- | The 'fromRight' function extracts the element out of a 'Right' and
+--   throws an error if its argument is 'Left'.
+--   Much like 'fromJust', using this function in polished code is usually a bad idea.
+fromRight :: Either l r -> r
 fromRight (Right x) = x
 
 #if __GLASGOW_HASKELL__ < 708
@@ -16,5 +25,7 @@ isLeft Left{} = True; isLeft _ = False
 isRight Right{} = True; isRight _ = False
 #endif
 
+-- | Pull the value out of an 'Either' where both alternatives
+--   have the same type.
 fromEither :: Either a a -> a
 fromEither = either id id
