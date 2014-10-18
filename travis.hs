809d97fed5e898823895e04e8496155d48c67479
Neil Mitchell <ndmitchell@gmail.com>
2014-10-18 13:31:40 +0100

    Tweak the travis.hs to have less junk in it

--- travis.hs
+++ travis.hs
@@ -1,10 +1,7 @@
 
-import Control.Monad
-import System.IO.Extra
 import System.Process.Extra
 
 main = do
-    let files = ["src/Extra.hs","test/TestGen.hs"]
     system_ "runhaskell -isrc Generate"
     system_ "git diff --exit-code"
     system_ "runhaskell -isrc -itest Test"
