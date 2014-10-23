772da815c50506b3f72fcf98f1464b5e888c7f74
Neil Mitchell <ndmitchell@gmail.com>
2014-10-23 17:55:40 +0100

    Remove all operations on triples apart from extractors

--- src/Data/Tuple/Extra.hs
+++ src/Data/Tuple/Extra.hs
@@ -8,8 +8,8 @@ module Data.Tuple.Extra(
     first, second, (***), (&&&),
     -- * More pair operations
     dupe, both,
-    -- * Operations on triples
-    fst3, snd3, thd3, first3, second3, third3, dupe3, both3,
+    -- * Extract from a triple
+    fst3, snd3, thd3
     ) where
 
 import Data.Tuple
@@ -45,21 +45,3 @@ snd3 (a,b,c) = b
 
 thd3 :: (a,b,c) -> c
 thd3 (a,b,c) = c
-
--- | Update the first component of a triple.
-first3 :: (a -> a') -> (a, b, c) -> (a', b, c)
-first3 f (a,b,c) = (f a, b, c)
-
--- | Update the second component of a triple.
-second3 :: (b -> b') -> (a, b, c) -> (a, b', c)
-second3 f (a,b,c) = (a, f b, c)
-
--- | Update the third component of a triple.
-third3 :: (c -> c') -> (a, b, c) -> (a, b, c')
-third3 f (a,b,c) = (a, b, f c)
-
-dupe3 :: a -> (a,a,a)
-dupe3 x = (x,x,x)
-
-both3 :: (a -> b) -> (a, a, a) -> (b, b, b)
-both3 f (x,y,z) = (f x, f y, f z)
