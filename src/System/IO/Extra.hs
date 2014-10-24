38f4a723151af7424a8886bed7b43c8a69d67dee
Neil Mitchell <ndmitchell@gmail.com>
2014-10-24 11:29:45 +0100

    Add comments to System.IO

--- src/System/IO/Extra.hs
+++ src/System/IO/Extra.hs
@@ -23,15 +23,18 @@ import Data.Time.Clock
 
 -- File reading
 
+-- | Like 'readFile', but setting an encoding.
 readFileEncoding :: TextEncoding -> FilePath -> IO String
 readFileEncoding enc file = do
     h <- openFile file ReadMode
     hSetEncoding h enc
     hGetContents h
 
+-- | Like 'readFile', but with the encoding 'utf8'.
 readFileUTF8 :: FilePath -> IO String
 readFileUTF8 = readFileEncoding utf8
 
+-- | Like 'readFile', but for binary files.
 readFileBinary :: FilePath -> IO String
 readFileBinary file = do
     h <- openBinaryFile file ReadMode
@@ -39,12 +42,16 @@ readFileBinary file = do
 
 -- Strict file reading
 
+-- | A strict version of 'readFile'. When the string is produced, the entire
+--   file will have been read into memory and the file handle will have been closed.
+--   Closing the file handle does not rely on the garbage collector.
 readFile' :: FilePath -> IO String
 readFile' file = withFile file ReadMode $ \h -> do
     s <- hGetContents h
     evaluate $ length s
     return s
 
+-- | A strict version of 'readFileEncoding', see 'readFile'' for details.
 readFileEncoding' :: TextEncoding -> FilePath -> IO String
 readFileEncoding' e file = withFile file ReadMode $ \h -> do
     hSetEncoding h e
@@ -52,9 +59,11 @@ readFileEncoding' e file = withFile file ReadMode $ \h -> do
     evaluate $ length s
     return s
 
+-- | A strict version of 'readFileUTF8', see 'readFile'' for details.
 readFileUTF8' :: FilePath -> IO String
 readFileUTF8' = readFileEncoding' utf8
 
+-- | A strict version of 'readFileBinary', see 'readFile'' for details.
 readFileBinary' :: FilePath -> IO String
 readFileBinary' file = withBinaryFile file ReadMode $ \h -> do
     s <- hGetContents h
@@ -63,14 +72,17 @@ readFileBinary' file = withBinaryFile file ReadMode $ \h -> do
 
 -- File writing
 
+-- | Write a file with a particular encoding.
 writeFileEncoding :: TextEncoding -> FilePath -> String -> IO ()
 writeFileEncoding enc file x = withFile x WriteMode $ \h -> do
     hSetEncoding h enc
     hPutStr h x
 
+-- | Write a file with the 'utf8' encoding.
 writeFileUTF8 :: FilePath -> String -> IO ()
 writeFileUTF8 = writeFileEncoding utf8
 
+-- | Write a binary file.
 writeFileBinary :: FilePath -> String -> IO ()
 writeFileBinary file x = withBinaryFile file WriteMode $ \h -> hPutStr h x
 
@@ -97,6 +109,8 @@ captureOutput act = withTempFile $ \file -> do
                 hSetBuffering out buf
 
 
+-- | Execute an action with a custom 'BufferMode', a warpper around
+--   'hSetBuffering'.
 withBuffering :: Handle -> BufferMode -> IO a -> IO a
 withBuffering h m act = bracket (hGetBuffering h) (hSetBuffering h) $ const $ do
     hSetBuffering h m
@@ -104,6 +118,9 @@ withBuffering h m act = bracket (hGetBuffering h) (hSetBuffering h) $ const $ do
 
 -- Temporary file
 
+-- | Provide a function to create a temporary file, and a way to delete a
+--   temporary file. Most users should use 'withTempFile' which
+--   combines these operations.
 newTempFile :: (IO FilePath, FilePath -> IO ())
 newTempFile = (create, ignore . removeFile)
     where
@@ -114,9 +131,16 @@ newTempFile = (create, ignore . removeFile)
             return file
 
 
+-- | Create a temporary file in the temporary directory. The file will be deleted
+--   after the action completes. The 'FilePath' will not have any file extension.
+--   If you require a file with a specific name, use 'withTempDir'.
 withTempFile :: (FilePath -> IO a) -> IO a
 withTempFile = uncurry bracket newTempFile
 
+
+-- | Provide a function to create a temporary directory, and a way to delete a
+--   temporary directory. Most users should use 'withTempDir' which
+--   combines these operations.
 newTempDir :: (IO FilePath, FilePath -> IO ())
 newTempDir = (create, ignore . removeDirectoryRecursive)
     where
@@ -133,5 +157,7 @@ newTempDir = (create, ignore . removeDirectoryRecursive)
                 \e -> find tmpdir (x+1)
 
 
+-- | Create a temporary directory inside the system temporary directory.
+--   The directory will be deleted after the action completes.
 withTempDir :: (FilePath -> IO a) -> IO a
 withTempDir = uncurry bracket newTempDir
