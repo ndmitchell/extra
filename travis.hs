481efe8c18b4b08deecc765664853bf101681b4d
Neil Mitchell <ndmitchell@gmail.com>
2014-10-29 07:47:20 +0000

    Simplify the travis script, just run the generator, everything is done automatically

--- travis.hs
+++ travis.hs
@@ -1,7 +1,4 @@
 
 import System.Process.Extra
 
-main = do
-    system_ "runhaskell -isrc Generate"
-    system_ "git diff --exit-code"
-    system_ "runhaskell -isrc -itest Test"
+main = system_ "runhaskell -isrc Generate"
