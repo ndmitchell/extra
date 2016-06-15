fe8a65a36e11d2d0f0f80afb7edd512e8db13497
Neil Mitchell <ndmitchell@gmail.com>
2016-06-15 10:15:39 +0100

    Move the instances over from Data.Typeable

--- src/Data/Typeable/Extra.hs
+++ src/Data/Typeable/Extra.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, ScopedTypeVariables, TypeOperators, GADTs #-}
+{-# LANGUAGE CPP, ScopedTypeVariables, TypeOperators, GADTs, StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 
 -- | This module extends "Data.Typeable" with extra functions available in later GHC versions.
@@ -11,6 +11,11 @@ module Data.Typeable.Extra(
 import Data.Typeable
 
 #if __GLASGOW_HASKELL__ < 708
+
+import Data.Ix
+import Control.Monad
+import Control.Applicative
+
 
 -- | Takes a value of type @a@ and returns a concrete representation
 --   of that type.
@@ -29,7 +34,92 @@ infix 4 :~:
 data a :~: b where
     Refl :: a :~: a
 
+deriving instance Eq   (a :~: b)
+deriving instance Show (a :~: b)
+deriving instance Ord  (a :~: b)
+
+instance a ~ b => Read (a :~: b) where
+  readsPrec d = readParen (d > 10) (\r -> [(Refl, s) | ("Refl",s) <- lex r ])
+
+instance a ~ b => Enum (a :~: b) where
+  toEnum 0 = Refl
+  toEnum _ = errorWithoutStackTrace "Data.Type.Equality.toEnum: bad argument"
+
+  fromEnum Refl = 0
+
+instance a ~ b => Bounded (a :~: b) where
+  minBound = Refl
+  maxBound = Refl
+
+
+
 -- | A canonical proxy type
 data Proxy t = Proxy
+
+-- It's common to use (undefined :: Proxy t) and (Proxy :: Proxy t)
+-- interchangeably, so all of these instances are hand-written to be
+-- lazy in Proxy arguments.
+
+instance Eq (Proxy s) where
+  _ == _ = True
+
+instance Ord (Proxy s) where
+  compare _ _ = EQ
+
+instance Show (Proxy s) where
+  showsPrec _ _ = showString "Proxy"
+
+instance Read (Proxy s) where
+  readsPrec d = readParen (d > 10) (\r -> [(Proxy, s) | ("Proxy",s) <- lex r ])
+
+errorWithoutStackTrace = error
+
+instance Enum (Proxy s) where
+    succ _               = errorWithoutStackTrace "Proxy.succ"
+    pred _               = errorWithoutStackTrace "Proxy.pred"
+    fromEnum _           = 0
+    toEnum 0             = Proxy
+    toEnum _             = errorWithoutStackTrace "Proxy.toEnum: 0 expected"
+    enumFrom _           = [Proxy]
+    enumFromThen _ _     = [Proxy]
+    enumFromThenTo _ _ _ = [Proxy]
+    enumFromTo _ _       = [Proxy]
+
+instance Ix (Proxy s) where
+    range _           = [Proxy]
+    index _ _         = 0
+    inRange _ _       = True
+    rangeSize _       = 1
+
+instance Bounded (Proxy s) where
+    minBound = Proxy
+    maxBound = Proxy
+
+instance Monoid (Proxy s) where
+    mempty = Proxy
+    mappend _ _ = Proxy
+    mconcat _ = Proxy
+
+instance Functor Proxy where
+    fmap _ _ = Proxy
+    {-# INLINE fmap #-}
+
+instance Applicative Proxy where
+    pure _ = Proxy
+    {-# INLINE pure #-}
+    _ <*> _ = Proxy
+    {-# INLINE (<*>) #-}
+
+instance Alternative Proxy where
+    empty = Proxy
+    {-# INLINE empty #-}
+    _ <|> _ = Proxy
+    {-# INLINE (<|>) #-}
+
+instance Monad Proxy where
+    _ >>= _ = Proxy
+    {-# INLINE (>>=) #-}
+
+instance MonadPlus Proxy
 
 #endif
