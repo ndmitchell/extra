68a66b12f1bd4640de2bd633b851dc57de8f1e8d
Melanie Brown <18196871+mel-brown@users.noreply.github.com>
2020-11-28 20:36:30 -0500

    New module Data.Bool.Extra

---
+++ src/Data/Bool/Extra.hs
@@ -0,0 +1,27 @@
+module Data.Bool.Extra (
+    module Data.Bool,
+    justIf,
+    (<?),
+    (?>),
+) where
+
+import Data.Bool
+
+infix 1 <?, ?>, `justIf`
+
+-- | Return either the value wrapped in a `Just`, or `Nothing` if the condition is false.
+--
+-- `(<?)` has a lower fixity than both `(&&)` and `(||)`, to avoid overbracketing.
+--
+-- > val `justIf` True == Just val
+-- > val `justIf` False == Nothing
+justIf :: a -> Bool -> Maybe a
+justIf a b = if b then Just a else Nothing
+
+-- | Infix variant of `justIf`.
+(<?) :: a -> Bool -> Maybe a
+(<?) = justIf
+
+-- | Flipped variant of `(<?)`, with the same fixity.
+(?>) :: Bool -> a -> Maybe a
+(?>) = flip (<?)
