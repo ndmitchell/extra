3f5f0f91986711bcf84ff425213313c775b4414b
Melanie Brown <brown.m@pm.me>
2021-03-06 09:09:53 -0500

    Data.Function.Extra.applyN

--- src/Data/Function/Extra.hs
+++ src/Data/Function/Extra.hs
@@ -4,7 +4,12 @@ module Data.Function.Extra (
 ) where
 
 import Data.Function
+import Data.Monoid (Endo (..))
+import Data.Semigroup (stimesMonoid)
 
 -- | Like 'on', but avoids recalculating values. Especially useful in folds.
 on2 :: (a -> a -> c) -> (b -> a) -> b -> b -> c
 (•) `on2` f = \x -> let fx = f x in (fx •) . f
+
+applyN :: Integral n => n -> (a -> a) -> (a -> a)
+applyN n = appEndo . stimesMonoid n . Endo
